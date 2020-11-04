e1714cfd4991c4c000e183e42026b8b6e4fc2cea
Wayne <waynebonnici@hotmail.com>
2020-11-05 00:16:31 +0100

    Added displaying of boot file name

--- Reviv/MainWindow.xaml.cs
+++ Reviv/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Reviv
 {
@@ -38,7 +26,8 @@ namespace Reviv
             {
                 _DumpFilePath = openFileDialog.FileName;
 
-                Revive.Visibility = Visibility.Visible;
+                DumpFileLabel.Content = Path.GetFileName(_DumpFilePath);
+                Riviv.Visibility = Visibility.Visible;
             }
         }
     }
